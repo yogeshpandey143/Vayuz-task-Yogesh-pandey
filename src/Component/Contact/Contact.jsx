e3059305492d08a3882ba1c3456a18d0197f1633
yogesh pandey <ygi21bc@gmail.com>
2025-07-05 00:41:28 +0530

    footer fix and contact fixed

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
   return (
-      <div className="contact-form">
+    <div className="contact-form">
       <h3>Connect With Us</h3>
       <form>
-        <input type="text" placeholder="Full Name" />
-        <input type="email" placeholder="Email" />
-        <input type="text" placeholder="Phone Number" />
+        <label htmlFor="name">Full Name</label>
+        <input type="text"  required />
+        <label htmlFor="email">Email</label>
+        <input type="email"  required />
+        <label htmlFor="phone">Phone Number</label>
+        <input type="text" required />
         <button type="submit">Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
+
+
--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './Footer.css';
+import Contact from '../Contact/Contact';
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-left">
+        
         <div className="footer-links">
           <a href="#">Events</a>
           <a href="#">Gen AI</a>
           <a href="#">Careers</a>
           <a href="#">Case Study</a>
           <a href="#">SME Talks</a>
-        </div>
+        </div> 
         <div className='footer-description'>
        <p>For any collaborative projects or enquiries feel free to connect with us.</p>
         <p className="website">vayuz.com</p>
@@ -25,17 +27,7 @@ const Footer = () => {
       </div>
 
       <div className="contact-form">
-          <h2>Connect With Us</h2>
-        <label>Full Name</label>
-        <input type="text" placeholder="Your Name" />
-
-        <label>Email</label>
-        <input type="email" placeholder="Your Email" />
-
-        <label>Phone Number</label>
-        <input type="text" placeholder="Your Phone" />
-
-        <button>Submit</button>
+          <Contact />
       </div>
     </div>
   );
